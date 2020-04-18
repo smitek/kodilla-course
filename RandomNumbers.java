c82e2649e2c0e5664aa3b3a02bd5983f2ca2ac34
smitek <63375703+smitek@users.noreply.github.com>
2020-04-18 10:53:29 +0200

    Add files via upload

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -1,17 +1,10 @@
+import java.util.Random;
+
 public class RandomNumbers {
     public static void main(String[] args){
-        int a = 0;
-        int suma = 0;
+        Random r = Random();
+        int a = r.nextInt(31);
+        System.out.println(a);
 
-        for (int i=0; i<=8; i++){
-            if (i == 0) {
-                //System.out.println(i);
-                //System.out.println("*******");
-               //a++;
-               //System.out.println("ilość " + a);
-               suma = suma + i;
-               System.out.println("Suma: " + suma);
             }
-        }
-    }
 }
