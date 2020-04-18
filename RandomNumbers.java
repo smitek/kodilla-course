7e2c0fe28bf6686cea4086ed50c2e3c17f79836c
smitek <63375703+smitek@users.noreply.github.com>
2020-04-18 10:55:34 +0200

    Add files via upload

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class RandomNumbers {
     public static void main(String[] args){
-        Random r = Random();
+        Random r = new Random();
         int a = r.nextInt(31);
         System.out.println(a);
 
