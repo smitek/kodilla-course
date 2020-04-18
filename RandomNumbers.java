b9f36811ede90604f2f889f326712a319d1f36bb
smitek <63375703+smitek@users.noreply.github.com>
2020-04-18 10:26:29 +0200

    Add files via upload

---
+++ RandomNumbers.java
@@ -0,0 +1,17 @@
+public class RandomNumbers {
+    public static void main(String[] args){
+        int a = 0;
+        int suma = 0;
+
+        for (int i=0; i<=8; i++){
+            if (i == 0) {
+                //System.out.println(i);
+                //System.out.println("*******");
+               //a++;
+               //System.out.println("ilość " + a);
+               suma = suma + i;
+               System.out.println("Suma: " + suma);
+            }
+        }
+    }
+}
---
+++ User.java
@@ -0,0 +1,24 @@
+public class User {
+    public String User;
+    public int age;
+
+    public User(String User, int age) {
+        this.User = User;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        User tomek = new User("Tomek", 31);
+        User bartek = new User("Bartek", 18);
+        User magda = new User("Magda", 19);
+        User szymon = new User("Szymon", 6);
+        User karol = new User("Karol", 3);
+        User[] users = {tomek, bartek, magda, szymon, karol};
+
+        int result = 0;
+        for (int i=0; i<10; i++){
+            result = result + age[i];
+        }
+ring[] User = new String[] {"SA", "DF", "LL"};
+    }
+}
