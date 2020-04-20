79c83659b444bc6bfd5bea4e372dd073efb5f87e
smitek <63375703+smitek@users.noreply.github.com>
2020-04-21 00:59:54 +0200

    Add files via upload

--- User.java
+++ User.java
@@ -6,9 +6,7 @@ public class User {
         this.userName = userName;
         this.age = age;
     }
-
     public static void main(String[] args) {
-        //String[] users = new String[5];
         User tomek = new User("Tomek", 31);
         User bartek = new User("Bartek", 18);
         User magda = new User("Magda", 19);
@@ -19,23 +17,14 @@ public class User {
         int result = 0;
         for (int i = 0; i < 5; i++) {
             result = result + users[i].age;
-            //System.out.println(result); wyświetla sumę age
+//            System.out.println(result); //wyświetla sumę age
         }
         int avg = result / users.length;
         System.out.println("średni wiek to " + avg + " lat");
 
-        for (int i=0; i<users.length; i++) {
-            if (> i);
-
-            System.out.println(users[i]);
-
-        }
+        for (int i = 0; i <users.length; i++)
+            if (users[0].age > avg) {
+                System.out.println(users[i]); // wyśw jakieś dziwne nazwy
+           }
     }
-//
-//        for (int i=0; i>avg; i++) {
-//            if ()
-        }
-
-        /*jeżeli wiek usera jest mniejszy od średniej wyświetl
-                users[0].age<avg ----- w for
-       */
+}
